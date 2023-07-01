53e33c05dd595215d746757a442be97405b5619c
mikronman <mkleczkajr@gmail.com>
2023-07-01 01:28:24 -0400

    cleaned up mobile UI a little

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
-import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('window:scroll', ['$event'])
@@ -18,4 +18,17 @@ export class NavbarComponent {
       this.renderer.removeClass(element, 'scrolled');
     }
   }
+  ngOnInit() {
+    const tabLinks = document.querySelectorAll('.nav-link');
+    const navbarCollapse = document.getElementById('navbarNav');
+  
+    tabLinks.forEach(tabLink => {
+      tabLink.addEventListener('click', () => {
+        if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+          this.renderer.removeClass(navbarCollapse, 'show');
+        }
+      });
+    });
+  }
+  
 }
